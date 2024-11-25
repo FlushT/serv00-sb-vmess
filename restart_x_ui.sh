0aef6d1b2b1ebae7c7dc0a9eb830331fc20818d9
flusht <149285239+flusht@users.noreply.github.com>
2024-11-25 21:04:47 +0800

    Delete restart_x_ui.sh

--- restart_x_ui.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# 检查进程是否在运行
-pgrep -x "x-ui" > /dev/null
-
-# 如果没有运行，则启动 x-ui
-if [ $? -ne 0 ]; then
-    pgrep -x x-ui > /dev/null || nohup /home/FlushT/x-ui/x-ui.sh restart >/dev/null 2>&1 &
-fi
