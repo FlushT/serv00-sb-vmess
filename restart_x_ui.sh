4c04e48cfc1c7f2e19df96021c0ea681ede6fa7f
flusht <149285239+flusht@users.noreply.github.com>
2024-11-25 14:22:08 +0800

    Update restart_x_ui.sh

--- restart_x_ui.sh
+++ restart_x_ui.sh
@@ -5,5 +5,5 @@ pgrep -x "x-ui" > /dev/null
 
 # 如果没有运行，则启动 x-ui
 if [ $? -ne 0 ]; then
-    /home/${USER}/x-ui/x-ui.sh restart
+    pgrep -x x-ui > /dev/null || nohup /home/FlushT/x-ui/x-ui.sh restart >/dev/null 2>&1 &
 fi
